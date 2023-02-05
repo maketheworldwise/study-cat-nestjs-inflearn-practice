afad1852e7506a10dcbfb3154693dd1583ec69a6
Kevin Ahn <maketheworldwise@gmail.com>
2023-02-05 15:47:27 +0900

    Refactoring: API 문서 및 Secret 보안 향상

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,27 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as expressBasicAuth from 'express-basic-auth';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.SERVER_PORT;
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: true,
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
-  const port = process.env.SERVER_PORT;
+
+  app.use(
+    ['/api', 'api-json'],
+    expressBasicAuth({
+      challenge: true,
+      users: {
+        [process.env.SWAGGER_USER]: process.env.SWAGGER_PASSWORD,
+      },
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Cats')
